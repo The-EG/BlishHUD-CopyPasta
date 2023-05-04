f582d6e4fb9fc9efba0d6b613f68e01412c332ec
Taylor Talkington <taylor.talkington@gmail.com>
2023-05-04 08:51:55 -0400

    🎉 Initial commit

---
+++ src/Copy Pasta/CopyPasta.cs
@@ -0,0 +1,124 @@
+﻿using Blish_HUD;
+using Blish_HUD.Controls;
+using Blish_HUD.Modules;
+using Blish_HUD.Modules.Managers;
+using Blish_HUD.Settings;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SharpDX.Text;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyPasta
+{
+    [Export(typeof(Module))]
+    public class CopyPasta : Module
+    {
+        private static readonly Logger logger = Logger.GetLogger<CopyPasta>();
+
+        internal ContentsManager contentsManager => this.ModuleParameters.ContentsManager;
+        internal DirectoriesManager directoriesManager => this.ModuleParameters.DirectoriesManager;
+
+        [ImportingConstructor]
+        public CopyPasta([Import("ModuleParameters")] ModuleParameters moduleParameters) : base(moduleParameters) {
+            moduleInstance = this;
+        }
+
+        protected override void DefineSettings(SettingCollection settings) {
+            /*
+            settings.DefineSetting(
+                "ExampleSetting",
+                "Default value",
+                () => "Display Name",
+                () => "Description");
+            */
+        }
+
+        protected override async Task LoadAsync() {
+            await LoadPasta();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            
+        }
+
+        protected override void OnModuleLoaded(EventArgs e) {
+            moduleIcon = new CornerIcon() {
+                Icon = contentsManager.GetTexture(@"copy-pasta.png"),
+                Priority = "CopyPasta".GetHashCode()
+            };
+
+            moduleIcon.Click += delegate {
+                pastaMenu.Show(moduleIcon);
+            };
+
+            /*
+            contextMenu = new ContextMenuStrip();
+            contextMenu.AddMenuItem("Reload Pasta");
+            moduleIcon.Menu = contextMenu;
+            */
+            base.OnModuleLoaded(e);
+        }
+
+        protected override void Unload() {
+            moduleInstance = null;
+        }
+
+        private void parsePasta(JObject json, ContextMenuStrip menu) {
+            foreach (JProperty prop in json.Properties()) {
+                string name = prop.Name;
+                ContextMenuStripItem item = new ContextMenuStripItem(name);
+
+                JToken val = prop.Value;
+                if (val.Type == JTokenType.Object) {
+                    ContextMenuStrip subMenu = new ContextMenuStrip();
+                    parsePasta((JObject)val, subMenu);
+                    item.Submenu = subMenu;
+                } else if (val.Type == JTokenType.String) {
+                    item.BasicTooltipText = val.ToString();
+
+                    item.Click += delegate {
+                        ClipboardUtil.WindowsClipboardService.SetUnicodeBytesAsync(System.Text.Encoding.Unicode.GetBytes(val.ToString()));
+                    };
+
+                } else {
+                    logger.Error("Can't parse json, expected array or string.");
+                    return;
+                }
+
+                menu.AddMenuItem(item);
+            }
+        }
+
+        private async Task LoadPasta() {
+            string pastaFolder = directoriesManager.GetFullDirectoryPath("copypasta");
+            pastaMenu = new ContextMenuStrip();
+
+            logger.Info($"Loading Pasta from {pastaFolder}...");
+
+            string[] files = Directory.GetFiles(pastaFolder, "*.json");
+
+            foreach ( string file in files ) {
+                using (StreamReader reader = File.OpenText(file)) {
+                    try {
+                        JObject json = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+
+                        parsePasta(json, pastaMenu);
+                    } catch (Exception ex) {
+                        logger.Warn($"Couldn't load json from {file}.");
+                    }
+                }
+            }
+        }
+
+        internal static CopyPasta moduleInstance;
+
+        private CornerIcon moduleIcon;
+        private ContextMenuStrip pastaMenu;
+        private ContextMenuStrip contextMenu;
+    }
+}
